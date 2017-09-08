ae5146bda5e970f4ef48c98e80d6949593b80d57
bombagian <gianfranco.bombardieri@gmail.com>
2017-09-08 11:17:52 +0200

    app5c backend created

---
+++ app5c Bookstore/backend.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+def connect():
+	  conn = sqlite3.connect("books.db")
+	  cur = conn.cursor()
+	  cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+	  conn.commit()
+	  conn.close()
+
+
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)", (title,author,year,isbn)) 
+    #need to pass the NULL value to allow SQL to automatically insert and auto-inrement the ID
+
+    conn.commit()
+    conn.close()
+
+
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+
+
+def search(title="", author="", year="", isbn=""):
+	#note the variables passed are "initialised to empty"
+	conn = sqlite3.connect("books.db")
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+	rows = cur.fetchall()
+	conn.close()
+	return rows
+
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update (id, title, author, year, isbn):
+	conn = sqlite3.connect("books.db")
+	cur = conn.cursor()
+	cur.execute("UPDATE book SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,year,isbn,id))
+	conn.commit()
+	conn.close()
+
+
+
+
+connect()
+#insert("Topolino","bomba",1985,12345336)
+#delete(7)
+update(5,"nuovo titolo","nuovo autore",1999,999)
+#update(2,"new title","gian",1987,9999)
+print(view())
+#print(search(author="gian"))
+
+
---
+++ app5c Bookstore/frontend.py
@@ -0,0 +1,70 @@
+from tkinter import *
+import backend.py
+
+
+window=Tk()
+window.title("GB's Bookstore")
+
+#create labels
+title_lbl=Label(window,text="Title")
+title_lbl.grid(row=0, column=0)
+title_txt=StringVar() 
+entry=Entry(window, textvariable = title_txt)
+entry.grid(row=0,column=1)
+
+
+author_lbl=Label(window,text="Author")
+author_lbl.grid(row=0, column=2)
+author_txt=StringVar() 
+entry=Entry(window, textvariable = author_txt)
+entry.grid(row=0,column=3)
+
+
+year_lbl=Label(window,text="Year")
+year_lbl.grid(row=1, column=0)
+year_txt=StringVar() 
+entry=Entry(window, textvariable = year_txt)
+entry.grid(row=1,column=1)
+
+
+isbn_lbl=Label(window,text="ISBN")
+isbn_lbl.grid(row=1, column=2)
+isbn_txt=StringVar() 
+entry=Entry(window, textvariable = isbn_txt)
+entry.grid(row=1,column=3)
+
+
+#Listbox
+list1=Listbox(window, height=8,width=40)
+list1.grid(row=2,column=0, rowspan=7, columnspan=2)
+
+#scrollbar
+sb1=Scrollbar(window)
+sb1.grid(row=2,column=2, rowspan=7)
+
+#configure the scroll assigning the list
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+
+
+#create button
+btn=Button(window,text="View All",width=12)
+btn.grid(row=3,column=3)
+#create button
+btn=Button(window,text="Search Entry",width=12)
+btn.grid(row=4,column=3)
+#create button
+btn=Button(window,text="Add Entry",width=12)
+btn.grid(row=5,column=3)
+#create button
+btn=Button(window,text="Update",width=12)
+btn.grid(row=6,column=3)
+#create button
+btn=Button(window,text="Delete",width=12)
+btn.grid(row=7,column=3)
+
+btn=Button(window,text="Close",width=12)
+btn.grid(row=8,column=3)
+
+window.mainloop()
